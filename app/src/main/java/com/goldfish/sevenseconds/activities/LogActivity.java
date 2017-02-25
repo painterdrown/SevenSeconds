cc01d16a0efec4eafe7bc449bdfc22aae97048ab
zzz0906 <zzz879978@outlook.com>
2017-02-26 04:26:47 +0800

    成功链接服务器
    public void Exception()
    其中这个用来停止线程报错
    也就是在ui线程跑异步消息 等待服务器给回参数才开始 感觉登录可以这么做因为这个是必须的

--- app/src/main/java/com/goldfish/sevenseconds/activities/LogActivity.java
+++ app/src/main/java/com/goldfish/sevenseconds/activities/LogActivity.java
@@ -1,8 +1,11 @@
 package com.goldfish.sevenseconds.activities;
 
+import android.app.Notification;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,8 +28,18 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class LogActivity extends AppCompatActivity {
-
+    private String user;
+    private String psw;
     private boolean check;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message message){
+            switch (message.what){
+                case 0: check = false;
+                case 1: check = true;
+            }
+        }
+    };
     public void Exception(){
         //避免出现android.os.NetworkOnMainThreadException异常
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
@@ -44,7 +57,7 @@ public class LogActivity extends AppCompatActivity {
 
         Connector.getDatabase();
         super.onCreate(savedInstanceState);
-        //Exception();
+        Exception();
         setContentView(R.layout.activity_log);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.hide();
@@ -56,8 +69,6 @@ public class LogActivity extends AppCompatActivity {
             public void onClick(View view) {
                 EditText edittextuser = (EditText) findViewById(R.id.editText1);
                 EditText edittextpsw = (EditText) findViewById(R.id.editText2);
-                String user;
-                String psw;
                 user = edittextuser.getText().toString();
                 psw = edittextpsw.getText().toString();
                 ProgressDialog progressDialog = new ProgressDialog(LogActivity.this);
@@ -75,8 +86,9 @@ public class LogActivity extends AppCompatActivity {
                 */
 
 
-                check = true;
-                //compare_user(user, psw);
+                check = false;
+                compare_user();
+                if (check == true) Log.d("test","true");else Log.d("test","false");
                 if (check == true) {
                         /*LastUser llast = new LastUser();
                         llast.setName(user);
@@ -108,20 +120,23 @@ public class LogActivity extends AppCompatActivity {
         });
     }
 
-    private void compare_user(String username, String password) {
+    private void compare_user() {
         try {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder().url("http://139.199.158.84:3000/api/signin").build();
             Response response = null;
             response = client.newCall(request).execute();
-            String reponseData = response.body().toString();
-            Log.d("test",reponseData);
+            String reponseData = response.body().string();
             Gson gson = new Gson();
             Users aa = gson.fromJson(reponseData, Users.class);
-            if (username.equals(aa.getUsername()) && password.equals(aa.getPassword())) check = true;
-        }
+            Message message = new Message();
+            if (aa.getUsername().equals(user) && aa.getPassword().equals(psw)) {
+                check = true;
+                message.what = 1;
+            } else {message.what = 0;}
+            }
         catch (IOException e) {
-                    e.printStackTrace();
-                }
+            e.printStackTrace();
+        }
     }
 }
