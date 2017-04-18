bddfd7836052244e5b32e058d67a30db3a2796ec
SYSUZZY <741323965@qq.com>
2017-04-19 01:52:45 +0800

    UI更改+注册检验

--- app/src/main/java/com/goldfish/sevenseconds/activities/LogActivity.java
+++ app/src/main/java/com/goldfish/sevenseconds/activities/LogActivity.java
@@ -82,63 +82,6 @@ public class LogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Exception();
         setContentView(R.layout.activity_log);
-        /*
-        Intent intent1 = new Intent(LogActivity.this, BarActivity.class);
-        startActivity(intent1);
-        */
-
-       /*Connector.getWritableDatabase();
-        MyFollow test = new MyFollow();
-        test.setName("世吹雀");
-        test.setAccount("noend22");
-        test.setIntroduction("大提琴/甜甜圈四重奏");
-        Resources res = getResources();
-        Bitmap bmp = ((BitmapDrawable) res.getDrawable(R.drawable.app_icon)).getBitmap();
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, os);
-        test.setFace(os.toByteArray());
-        test.save();
-
-        dbChattingDatabaseHelper = new ChattingDatabaseHelper(
-                this, "MessageStore.db", null, 1);
-        SQLiteDatabase dbMessage1 = dbChattingDatabaseHelper.getWritableDatabase();
-        dbMessage1.execSQL("create table if not exists noend22 ("
-                + "id integer primary key autoincrement, "
-                + "account text, "
-                + "message text, "
-                + "time text, "
-                + "sendOrReceive integer, "
-                + "readOrNot integer)");
-        dbMessage1.execSQL("insert into noend22 (account, message, time, sendOrReceive, readOrNot) " +
-                        "values(?, ?, ?, ?, ?)",
-                new String[]{"noend22", "Hey", "2017/2/24 10:32", "0", "0"});
-        dbMessage1.execSQL("insert into noend22 (account, message, time, sendOrReceive, readOrNot) " +
-                        "values(?, ?, ?, ?, ?)",
-                new String[]{"y741323965", "Hey! What's up!", "2017/2/24 10:32", "1", "1"});
-        dbMessage1.execSQL("insert into noend22 (account, message, time, sendOrReceive, readOrNot) " +
-                        "values(?, ?, ?, ?, ?)",
-                new String[]{"noend22", "Miss me?", "2017/2/24 10:33", "0", "0"});
-
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, os);
-        Information test1 = new Information();
-        test1.setFace(os.toByteArray());
-        test1.setSex("男");
-        test1.setIntroduction("穿睡服的金鱼/梦幻西游");
-        test1.setName("Goldfish");
-        test1.setAccount("y741323965");
-        test1.setBirthday("1997-1-2");
-        test1.setPhone("13719326474");
-        test1.save();
-
-        Information test2 = new Information();
-        test2.setPhone("13502852468");
-        test2.setBirthday("1997-1-1");
-        test2.setAccount("noend22");
-        test2.setName("世吹雀");
-        test2.setIntroduction("大提琴/甜甜圈四重奏");
-        test2.setSex("女");
-        test2.save();*/
-
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
 
         if (actionBar != null) actionBar.hide();
@@ -172,11 +115,10 @@ public class LogActivity extends AppCompatActivity {
             }
                             }).start();
         }
-        Button button_connect = (Button)findViewById(R.id.connect_us);
         Button button_log = (Button) findViewById(R.id.button_log);
         Button button_register = (Button) findViewById(R.id.button_register);
         Button button_return = (Button) findViewById(R.id.return_something);
-        EditText edittextuser = (EditText) findViewById(R.id.editText1);
+        EditText edittextuser = (EditText) findViewById(R.id.input_name);
         List<LastUser> lastUsers = DataSupport.findAll(LastUser.class);
         if (lastUsers.size() != 0){
             edittextuser.setText(lastUsers.get(0).getName());
@@ -185,8 +127,8 @@ public class LogActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                EditText edittextuser = (EditText) findViewById(R.id.editText1);
-                EditText edittextpsw = (EditText) findViewById(R.id.editText2);
+                EditText edittextuser = (EditText) findViewById(R.id.input_name);
+                EditText edittextpsw = (EditText) findViewById(R.id.input_password);
                 user = edittextuser.getText().toString();
                 psw = edittextpsw.getText().toString();
                 final ProgressDialog progressDialog = new ProgressDialog(LogActivity.this);
@@ -194,7 +136,6 @@ public class LogActivity extends AppCompatActivity {
                 progressDialog.setMessage("waiting");
                 progressDialog.setCancelable(false);
                 progressDialog.show();
-
 
                 new Thread(new Runnable() {
                     @Override
@@ -248,7 +189,7 @@ public class LogActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.sysu7s.cn/contact_us/"));
+                intent.setData(Uri.parse("http://www.sysu7s.cn/contact-us/"));
                 startActivity(intent);
             }
         });
